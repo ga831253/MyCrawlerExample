0b8b9f1d7989a1388fb312fcba6a90b778cfa142
ga831253 <ji3g4j6zo6@gmail.com>
2017-09-03 12:05:56 +0800

    BassicExamplenew

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -2,6 +2,7 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +21,16 @@ public class BasicExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "http://.../";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1504195288.A.804.html";
 
-		System.out.println(
-				CrawlerPack.start()
-				
-				// 參數設定
-			    //.addCookie("key","value")	// 設定cookie
+		Elements elem= CrawlerPack.start().addCookie("over18","1").getFromHtml(uri).select("#main-content");
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
-				
+
 				// 選擇資料格式 (三選一)
-				.getFromJson(uri)
-			    //.getFromHtml(uri)
-			    //.getFromXml(uri)
-			    
-			    // 這兒開始是 Jsoup Document 物件操作
-			    .select(".css .selector ")
-			    
-		);
+				//.getFromJson(uri)
+		elem.select("div").remove();
+				//.getFromXml(uri)
+
+		System.out.println(elem);
 	}
 }
